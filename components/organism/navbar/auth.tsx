e17e541c799fd3bd73b4c3207123023d9d2912f2
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-10-09 14:22:43 +0800

    membuat kondisi login di navbar dan membuat fungsi logout

--- components/organism/navbar/auth.tsx
+++ components/organism/navbar/auth.tsx
@@ -1,19 +1,51 @@
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Cookies from "js-cookie";
+import jwtDecode from 'jwt-decode';
+import { JWTPayloadTypes, UserTypes } from '../../../services/data-types';
+import { useRouter } from 'next/router';
 
-interface AuthProps {
-  isLogin?: boolean;
-}
-export default function Auth(props: Partial<AuthProps>) {
-  const { isLogin } = props;
+
+export default function auth() {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState({
+    avatar: ''
+  })
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get('token')
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const dataUserFromPayload: UserTypes = payload.player;
+
+      const IMG = process.env.NEXT_PUBLIC_IMG;
+      dataUserFromPayload.avatar = `${IMG}/${dataUserFromPayload.avatar}`
+
+      setUser(dataUserFromPayload)
+      setIsLogin(true);
+      // console.log('dataUserFromPayload: ', dataUserFromPayload);
+    }
+  }, [])
+
+  const onLogOut = () => {
+    Cookies.remove('token')
+    router.push('/')
+    setIsLogin(false)
+  }
+
   if (isLogin) {
     return (
       <li className="nav-item my-auto dropdown d-flex">
         <div className="vertical-line d-lg-block d-none"></div>
         <div>
           <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="/img/avatar-1.png" className="rounded-circle" width="40" height="40" alt="" />
+            data-bs-toggle="dropdown" aria-expanded="false"
+          >
+            <img src={user.avatar} className="rounded-circle" width="40" height="40" alt="" />
           </a>
           <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
             <li>
@@ -31,10 +63,8 @@ export default function Auth(props: Partial<AuthProps>) {
                 <a className="dropdown-item text-lg color-palette-2" href="#">Account Settings</a>
               </Link>
             </li>
-            <li>
-              <Link href="/sign-in">
-                <a className="dropdown-item text-lg color-palette-2" href="#">Log Out</a>
-              </Link>
+            <li onClick={onLogOut}>
+              <a className="dropdown-item text-lg color-palette-2">Log Out</a>
             </li>
 
           </ul>
