17edb66f12764dcc2afbff627f485326600e573e
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-10-13 17:04:22 +0800

    handle private URL

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,8 +2,19 @@ import Image from "next/image"
 import CheckoutConfirmation from "../components/organism/checkout-confirm"
 import CheckoutDetail from "../components/organism/checkout-detail"
 import CheckoutItem from "../components/organism/checkout-item"
+import jwtDecode from "jwt-decode"
+import { JWTPayloadTypes, UserTypes } from "../services/data-types"
 
-function Checkout() {
+interface CheckoutProps {
+  user: UserTypes;
+}
+
+function Checkout(props: CheckoutProps) {
+
+  const { user } = props;
+
+  // console.log('user: ', user);
+
   return (
     <>
       {/* <!-- Checkout Content --> */}
@@ -28,4 +39,37 @@ function Checkout() {
   )
 }
 
+
 export default Checkout
+
+export async function getServerSideProps({ req }) {
+
+  const { token } = req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, "base64").toString('ascii');
+
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const dataUserFromPayload: UserTypes = payload.player;
+
+  // console.log(jwtToken);
+
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  dataUserFromPayload.avatar = `${IMG}/${dataUserFromPayload.avatar}`
+
+
+
+  return {
+    props: {
+      user: dataUserFromPayload,
+    }
+  }
+}
